497970392db8a8c7bcef16f22dbcd102ca7f8fe2
jo2312al <sharkdark2312@gmail.com>
2025-03-24 12:47:51 -0600

    Initial commit

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => '1.0.0+no-version-set',
+        'version' => '1.0.0.0',
+        'reference' => null,
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => '1.0.0+no-version-set',
+            'version' => '1.0.0.0',
+            'reference' => null,
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'sergeytsalkov/meekrodb' => array(
+            'pretty_version' => 'v3.1.3',
+            'version' => '3.1.3.0',
+            'reference' => 'ec430091562e59d08ba67e3f85f5e767a519e215',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../sergeytsalkov/meekrodb',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
